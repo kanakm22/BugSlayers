b2643da42d0646bb39501f261a14a1d0e646bd69
Kanak Maurya <kanakmaurya22@gmail.com>
2025-10-02 11:49:43 +0530

    .

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 // Middleware
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/public')));
 
 // EJS setup
 app.set("view engine", "ejs");
